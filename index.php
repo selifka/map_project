fb49d458f0a2b716040285ffe58f5acb6a85bf2b
Selifka <sba37@wildcats.unh.edu>
2018-04-30 15:11:08 -0400

    Adding JS for zoom functionality and grabbing lat/lng data

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <?php
-	if(isset($_POST['submit']))
-	{
-		// These are the credentials for a test database on a test dev server, don't worry!
-		$connect = mysqli_connect("127.0.0.1", "root", "2bornot2B",  "mapdb");
+	// if(isset($_POST['submit']))
+	// {
+	// 	// These are the credentials for a test database on a test dev server, don't worry!
+	// 	$connect = mysqli_connect("127.0.0.1", "root", "2bornot2B",  "mapdb");
 
-		$s_name = $_POST['settlement_name'];
-		$mysql_query = "SELECT * FROM settlements WHERE name LIKE '%" .$s_name. "%'";
-		$query = mysqli_query($connect, $mysql_query);
+	// 	$s_name = $_POST['settlement_name'];
+	// 	$s_query = "SELECT * FROM settlements WHERE name LIKE '%" .$s_name. "%'";
+	// 	$result = mysqli_query($connect, $s_query);
 
-		while ($row = mysqli_fetch_assoc($query)) {
-			$county = $row['county_id'];
-			$district = $row['district_id'];
-		}
-	}
+	// 	if($result->num_rows) {
+	// 		while ($row = mysqli_fetch_assoc($result)) {
+	// 			$county = $row['county_id'];
+	// 			$district = $row['district_id'];
+	// 		}
+	// 	} else {
+	// 			echo "That name does not match any city in MS";
+	// 	}
+	// }
 ?>
 
 <!DOCTYPE html>
@@ -34,29 +38,41 @@
 
   <body>
     <div class="container">
-    	<h1 class="py-5 text-center">My Google Maps Demo</h1>
+    	<h1 class="py-3 text-center">My Google Maps Demo</h1>
 	  		<form action="index.php" method="post" class="text-center py-5">
 			    <div class="row">
 			    	<div class="col-md-3 py-3">
-			    		<label>Address or City of Event:</label>
+			    		<label>Address of Event:</label>
 			    	</div>
 			    	<div class="col-md-6 py-3">
-		        		<input class="form-control" type="text" name="settlement_name">
+		        		<input class="form-control" id="address" type="text" value="">
 		        	</div>
 		        	<div class="col-md-3 py-3">
-			      		<input type="submit" name="submit" value="Submit">
+			      		<input id="submit" type="button" value="Submit">
 			      	</div>
 		        	<div class="col-md-3 py-3">
-			    		<label>County:</label>
+			    		<label>County or Counties:</label>
 			    	</div>
 			      	<div class="col-md-9 py-3">
-		        		<input class="form-control" type="text" name="result" value="<?php echo $county;?>">
+		        		<input class="form-control" type="text" name="result" value="">
 		        	</div>
 		        	<div class="col-md-3 py-3">
-			    		<label>Ciruit Disctrict:</label>
+			    		<label>Ciruit Disctrict for this county:</label>
 			    	</div>
 			      	<div class="col-md-9 py-3">
-		        		<input class="form-control" type="text" name="result2" value="<?php echo $district;?>">
+		        		<input class="form-control" type="text" name="result2" value="">
+		        	</div>
+		        	<div class="col-md-3 py-3">
+		        		<label>Latitude:</label>
+		        	</div>
+		        	<div class="col-md-9 py-3">
+		        		<label id="lat"></label>
+		        	</div>
+		        	<div class="col-md-3 py-3">
+		        		<label>Longitude:</label>
+		        	</div>
+		        	<div class="col-md-9 py-3">
+		        		<label id="lng"></label>
 		        	</div>
 				</div>
 		  </form>
@@ -69,20 +85,50 @@
 
     <script>
     function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+        	zoom: 8,
+        	center: {lat: 32.7795, lng: -89.7031}
+        });
 
- 		var map = new google.maps.Map(document.getElementById('map'), {
-    		center: new google.maps.LatLng(32.7795,-89.7031),
-    		zoom: 8
-  		});
+        var geocoder = new google.maps.Geocoder();
 
-	    // KML Layer Import
+        document.getElementById('submit').addEventListener('click', function() {
+        	geocodeAddress(geocoder, map);
+        });
+
 		var kml_url = "https://s3.amazonaws.com/kmllayer/counties_ms.kml"; 
 
 		var Kmllayer = new google.maps.KmlLayer(kml_url, {
 		 	map: map
 		});
 
-	}
+    }
+
+    function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('address').value;
+
+        geocoder.geocode({'address': address}, function(results, status) {
+
+        	if (status === 'OK') {
+            	resultsMap.setCenter(results[0].geometry.location);
+
+            	// Display the lat and lng in html 
+            	document.getElementById('lat').innerHTML = results[0].geometry.location.lat();
+            	document.getElementById('lng').innerHTML = results[0].geometry.location.lng();
+
+            	var marker = new google.maps.Marker({
+              		map: resultsMap,
+              		position: results[0].geometry.location
+            	});
+
+            	resultsMap.setZoom(15);
+            	resultsMap.panTo(marker.position);
+
+          	} else {
+            	alert('Geocode was not successful for the following reason: ' + status);
+          	}
+        });
+    }
     </script>
 
   </body>
